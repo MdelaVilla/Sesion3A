78f53f1200fed593e6784550b41c2e51da6abd3a
usuario <usuario@pc20-030>
2023-10-10 10:55:24 +0200

    Ejercicio 2. TDD paso  y 2, elaboro caso de prueba y código mínimo.

---
+++ src/pkg/Cuenta.java
@@ -0,0 +1,25 @@
+package pkg;
+
+public class Cuenta {
+
+	public Cuenta(String numero, String titular, double saldo) {
+		super();
+		this.saldo = saldo;
+	}
+
+	double saldo;
+	
+	public double getSaldo() {
+		return 1000;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	public void ingresar(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
